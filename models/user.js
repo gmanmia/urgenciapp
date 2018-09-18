0b5796fd79c5925ed7eda632fedc812dab57d77d
German Rueda <contact.germanrueda@gmail.com>
2018-09-18 15:57:07 +0000

    update report and user models

--- models/user.js
+++ models/user.js
@@ -4,7 +4,12 @@ var mongoose = require("mongoose"),
 
 var userSchema = new mongoose.Schema({
     username: String,
-    password: String
+    password: String,
+    avatar: String,
+    firstName: String,
+    lastName: String,
+    email: String,
+    isAdmin: {type: Boolean, default: false}
 });
 
 userSchema.plugin(passportLocalMongoose);
--- routes/report.js
+++ routes/report.js
@@ -40,11 +40,63 @@ router.post("/facilities/:id/report", isLoggedIn, function(req, res){
     });
 });
 
+// EDIT - modify info about the report for any hospital
+router.get("/facilities/:id/report/:report_id/edit", checkReportOwnership, function(req, res){
+   Report.findById(req.params.report_id, function(err, foundReport){
+       if(err){
+           console.log(err);
+           res.redirect("/facilities");
+       } else {
+           res.render("reports/edit", {facility_id: req.params.id, report: foundReport});
+       }
+   });
+});
+
+// UPDATE - modify the comment based on user input
+router.put("/facilities/:id/report/:report_id", checkReportOwnership, function(req, res){
+    Report.findByIdAndUpdate(req.params.report_id, req.body.report, function(err, updatedReport){
+        if(err){
+            console.log(err);
+            res.redirect("/facilities");
+        } else {
+            res.redirect("/facilities/" + req.params.id)
+        }
+    });
+});
+
+// DESTROY - remove a report
+router.delete("/facilities/:id/report/:report_id", checkReportOwnership, function(req, res){
+   Report.findByIdAndRemove(req.params.report_id, function(err){
+       if(err){
+           res.redirect("/facilities/" + req.params.id);
+       } else {
+        //   req.flash("success", "Succesfully DELETED your report!");
+           res.redirect("/facilities/" + req.params.id);
+       }
+   });
+});
+
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
         return next();
     }
     res.redirect("/login");
+}
+
+function checkReportOwnership(req, res, next){
+    if(req.isAuthenticated()){
+     Report.findById(req.params.report_id, function(err, foundReport){
+         if(err){
+             res.redirect("back");
+         } else {
+             if(foundReport.alias.id.equals(req.user._id) || req.user.isAdmin){
+                 next();
+             } else {
+                 res.redirect("back");
+             }
+         }
+     })   
+    }
 }
 
 module.exports = router;
